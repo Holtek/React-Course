3e80513ab92fc8de891fa362677d7af562c9ab55
Nikola Obradovic <obradovicnik01@gmail.com>
2019-10-30 17:40:16 +0100

    jsx kinda sux

--- module-1/my-app/src/App.js
+++ module-1/my-app/src/App.js
@@ -5,8 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>hi am a react app</h1>
+      <p>jsx kinda sux</p>
     </div>
   );
+  // return React.createElement("div", null, "h1", "Hi i'm a react app");
 }
 
 export default App;
