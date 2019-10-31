32d50e99b5c1e842effde9d34fdce4888de183ad
Nikola Obradovic <obradovicnik01@gmail.com>
2019-10-31 15:44:37 +0100

    componets/props etc...

--- module-1/my-app/src/App.js
+++ module-1/my-app/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
-
+import Person from "./Person/person";
 function App() {
   return (
     <div className="App">
       <h1>hi am a react app</h1>
       <p>jsx kinda sux</p>
+      <Person name="Too" age="13"></Person>
+      <Person name="Boo" age="23">
+        My Hobbies: Racing
+      </Person>
+      <Person name="Goo" age="33"></Person>
     </div>
   );
   // return React.createElement("div", null, "h1", "Hi i'm a react app");
