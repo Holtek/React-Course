ecc927f1d645af2706119b771f069b0ba37f3947
Nikola Obradovic <obradovicnik01@gmail.com>
2019-11-03 19:48:17 +0100

    ...

--- module-1/my-app/src/App.js
+++ module-1/my-app/src/App.js
@@ -1,19 +1,20 @@
-import React from "react";
-import "./App.css";
-import Person from "./Person/person";
-function App() {
-  return (
-    <div className="App">
-      <h1>hi am a react app</h1>
-      <p>jsx kinda sux</p>
-      <Person name="Too" age="13"></Person>
-      <Person name="Boo" age="23">
-        My Hobbies: Racing
-      </Person>
-      <Person name="Goo" age="33"></Person>
-    </div>
-  );
-  // return React.createElement("div", null, "h1", "Hi i'm a react app");
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Hi, I'm a React App</h1>
+       <p>This is really working!</p>
+       <Person name="Max" age="28" />
+       <Person name="Manu" age="29" >My Hobbies: Racing</Person>
+       <Person name="Stephanie" age="26" />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
 export default App;
--- module-1/my-app/src/Person/person.js
+++ module-1/my-app/src/Person/person.js
@@ -1,13 +1,12 @@
-import React from "react";
-const person = props => {
-  return (
-    <div>
-      <p>
-        I am {props.name} and i am {props.age} years old
-      </p>
-      <p>{props.children}</p>
-    </div>
-  );
+import React from 'react';
+
+const person = ( props ) => {
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
